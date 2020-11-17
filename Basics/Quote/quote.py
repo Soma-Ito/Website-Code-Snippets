defcd5d144e1e9bfd02f41ab96a539032020b3c9
ProTechMaison <protechmaison@gmail.com>
2020-11-17 23:02:16 +0900

    Basics1 Quote

--- Basics/Quote/quote.py
+++ Basics/Quote/quote.py
@@ -6,10 +6,10 @@ print(quote)
 # quoteの中に"もしくは'を使う場合
 # 外側のquoteには内側と別の記号を使う必要があります
 # Errorとなるコード例
-# quote_in_quote_error = 'Who's dog?'
+# quote_in_quote_error = 'Who's he?'
 # print(quote_in_quote)
 
-quote_in_quote = "Who's dog?"
+quote_in_quote = "Who's he?"
 print(quote_in_quote)
 
 # 改行も考慮したquote
